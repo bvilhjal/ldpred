4d40cf8dce76fe62300efc03a104652824d5ab3b
Jojo Dijamco <joj@google.com>
2019-04-22 14:30:58 -0700

    Use Python unittest module in test.py

--- test.py
+++ test.py
@@ -9,8 +9,9 @@ import glob
 import gzip
 import h5py
 import numpy as np
+import os
 import tempfile
-import os    
+import unittest
 
 def run_test(mesg, cmd_str, error_mesg, *actual_and_golden_outputs):
   print(mesg)
@@ -135,102 +136,106 @@ def assert_files_equal(file1, file2):
     assert filecmp.cmp(file1, file2, "Mismatch between: %s and %s" % (file1, file2))
 
 
-tf = tempfile.NamedTemporaryFile()
-tmp_file_prefix = next(tempfile._get_candidate_names())
-try:
-    print('Testing LDpred.\n')
-    print('Note that this test currently only tests the core functionality of LDpred.')
-    print('Please report bugs on github (https://github.com/bvilhjal/ldpred) or to Bjarni J Vilhjalmsson (bjarni.vilhjalmsson@gmail.com).\n')
-    
-    coord_file = tmp_file_prefix + '.coord0.hdf5'
-    run_test(
-        'Coordinating test data into file %s' % coord_file,
-        'coord --gf=./test_data/LDpred_data_p0.001_train_0 --vgf=./test_data/LDpred_data_p0.001_test_0 --ssf=./test_data/LDpred_data_p0.001_ss_0.txt --ssf-format=STANDARD  --N=10000  --out=%s' % coord_file,
-        'Problems when coordinating data!',
-        coord_file,
-        'test_data/goldens/golden.coord0.hdf5'
-    )
-    
-    coord_file = tmp_file_prefix + '.coord.hdf5'
-    run_test(
-        'Coordinating test data into file %s' % coord_file,
-        '--debug coord --gf=./test_data/LDpred_data_p0.001_train_0 --vbim=./test_data/LDpred_data_p0.001_test_0.bim --ssf=./test_data/LDpred_data_p0.001_ss_0.txt --ssf-format=STANDARD  --beta --N=10000  --out=%s' % coord_file,
-        'Problems when coordinating data!',
-        coord_file,
-        'test_data/goldens/golden.coord.hdf5')
+class TestLDPred(unittest.TestCase):
+  def test_ldpred(self):
+    tf = tempfile.NamedTemporaryFile()
+    tmp_file_prefix = next(tempfile._get_candidate_names())
+    try:
+        print('Testing LDpred.\n')
+        print('Note that this test currently only tests the core functionality of LDpred.')
+        print('Please report bugs on github (https://github.com/bvilhjal/ldpred) or to Bjarni J Vilhjalmsson (bjarni.vilhjalmsson@gmail.com).\n')
 
-    run_test(
-        'Running LDpred-inf with coordinated file prefix: %s ' % tmp_file_prefix,
-        '--debug inf --cf=%s  --ldr=100   --ldf=%s  --N=10000  --out=%s' % (coord_file, tmp_file_prefix, tmp_file_prefix),
-        'Problems when running LDpred_inf!',
-        tmp_file_prefix + '.txt',
-        'test_data/goldens/golden.txt',
-        tmp_file_prefix + '_ldradius100.pkl.gz',
-        'test_data/goldens/golden_ldradius100.pkl.gz')
-    
-    run_test(
-        'Running LDpred with coordinated file prefix: %s ' % tmp_file_prefix,
-        '--debug gibbs --cf=%s  --ldr=100   --ldf=%s  --f=0.001 --N=10000  --out=%s' % (coord_file, tmp_file_prefix, tmp_file_prefix),
-        'Problems when running LDpred!',
-        tmp_file_prefix + '_LDpred-inf.txt',
-        'test_data/goldens/golden_LDpred-inf.txt',
-        tmp_file_prefix + '_LDpred_p1.0000e-03.txt',
-        'test_data/goldens/golden_LDpred_p1.0000e-03.txt')
-    
-    run_test(
-        'Running P+T with coordinated file prefix: %s ' % tmp_file_prefix,
-        '--debug p+t --cf=%s  --ldr=100  --p=0.001 --out=%s' % (coord_file, tmp_file_prefix),
-        'Problems when running P+T!',
-        tmp_file_prefix + '_P+T_r0.20_p1.0000e-03.txt',
-        'test_data/goldens/golden_P+T_r0.20_p1.0000e-03.txt')
-    
-    prs_file_prefix = tmp_file_prefix + 'prs'
-    run_test(
-        'Validating results with output file prefix: %s' % tmp_file_prefix,
-        '--debug score --gf=./test_data/LDpred_data_p0.001_test_0  --rf=%s  --out=%s' % (tmp_file_prefix, prs_file_prefix),
-        'Problems with the validation step!',
-        prs_file_prefix + '_LDpred-inf.txt',
-        'test_data/goldens/goldenprs_LDpred-inf.txt',
-        prs_file_prefix + '_LDpred_p1.0000e-03.txt',
-        'test_data/goldens/goldenprs_LDpred_p1.0000e-03.txt')
-    
-    prs_file_prefix2 = tmp_file_prefix + 'prs_2'
-    run_test(
-        'Validating results with output file prefix: %s' % tmp_file_prefix,
-        'score --gf=./test_data/LDpred_data_p0.001_test_0  --rf=%s  --out=%s' % (tmp_file_prefix, prs_file_prefix2),
-        'Problems with the validation step!',
-        prs_file_prefix2 + '_LDpred-inf.txt',
-        'test_data/goldens/goldenprs_2_LDpred-inf.txt',
-        prs_file_prefix2 + '_LDpred_p1.0000e-03.txt',
-        'test_data/goldens/goldenprs_2_LDpred_p1.0000e-03.txt',)
+        coord_file = tmp_file_prefix + '.coord0.hdf5'
+        run_test(
+            'Coordinating test data into file %s' % coord_file,
+            'coord --gf=./test_data/LDpred_data_p0.001_train_0 --vgf=./test_data/LDpred_data_p0.001_test_0 --ssf=./test_data/LDpred_data_p0.001_ss_0.txt --ssf-format=STANDARD  --N=10000  --out=%s' % coord_file,
+            'Problems when coordinating data!',
+            coord_file,
+            'test_data/goldens/golden.coord0.hdf5'
+        )
 
-    run_test(
-        'Validating results with output file prefix: %s' % tmp_file_prefix,
-        'score --gf=./test_data/LDpred_data_p0.001_test_0  --rf=%s  --rf-format=P+T --out=%s' % (tmp_file_prefix, prs_file_prefix),
-        'Problems with the P+T validation step!',
-        prs_file_prefix + '_P+T_p1.0000e-03.txt',
-        'test_data/goldens/goldenprs_2_P+T_p1.0000e-03.txt')
+        coord_file = tmp_file_prefix + '.coord.hdf5'
+        run_test(
+            'Coordinating test data into file %s' % coord_file,
+            '--debug coord --gf=./test_data/LDpred_data_p0.001_train_0 --vbim=./test_data/LDpred_data_p0.001_test_0.bim --ssf=./test_data/LDpred_data_p0.001_ss_0.txt --ssf-format=STANDARD  --beta --N=10000  --out=%s' % coord_file,
+            'Problems when coordinating data!',
+            coord_file,
+            'test_data/goldens/golden.coord.hdf5')
 
-    prs_file_prefix3 = tmp_file_prefix + 'prs_3'
-    run_test(
-        'Validating results with output file prefix: %s' % tmp_file_prefix,
-        'score --gf=./test_data/LDpred_data_p0.001_test_0  --only-score --rf=%s  --rf-format=P+T --out=%s' % (tmp_file_prefix, prs_file_prefix3),
-        'Problems with the P+T validation step!',
-        prs_file_prefix3 + '_P+T_p1.0000e-03.txt',
-        'test_data/goldens/goldenprs_3_P+T_p1.0000e-03.txt')
-    print('Test finished successfully!')
+        run_test(
+            'Running LDpred-inf with coordinated file prefix: %s ' % tmp_file_prefix,
+            '--debug inf --cf=%s  --ldr=100   --ldf=%s  --N=10000  --out=%s' % (coord_file, tmp_file_prefix, tmp_file_prefix),
+            'Problems when running LDpred_inf!',
+            tmp_file_prefix + '.txt',
+            'test_data/goldens/golden.txt',
+            tmp_file_prefix + '_ldradius100.pkl.gz',
+            'test_data/goldens/golden_ldradius100.pkl.gz')
 
+        run_test(
+            'Running LDpred with coordinated file prefix: %s ' % tmp_file_prefix,
+            '--debug gibbs --cf=%s  --ldr=100   --ldf=%s  --f=0.001 --N=10000  --out=%s' % (coord_file, tmp_file_prefix, tmp_file_prefix),
+            'Problems when running LDpred!',
+            tmp_file_prefix + '_LDpred-inf.txt',
+            'test_data/goldens/golden_LDpred-inf.txt',
+            tmp_file_prefix + '_LDpred_p1.0000e-03.txt',
+            'test_data/goldens/golden_LDpred_p1.0000e-03.txt')
 
-except Exception as e:
-    print("Test failed: ",e)
+        run_test(
+            'Running P+T with coordinated file prefix: %s ' % tmp_file_prefix,
+            '--debug p+t --cf=%s  --ldr=100  --p=0.001 --out=%s' % (coord_file, tmp_file_prefix),
+            'Problems when running P+T!',
+            tmp_file_prefix + '_P+T_r0.20_p1.0000e-03.txt',
+            'test_data/goldens/golden_P+T_r0.20_p1.0000e-03.txt')
+
+        prs_file_prefix = tmp_file_prefix + 'prs'
+        run_test(
+            'Validating results with output file prefix: %s' % tmp_file_prefix,
+            '--debug score --gf=./test_data/LDpred_data_p0.001_test_0  --rf=%s  --out=%s' % (tmp_file_prefix, prs_file_prefix),
+            'Problems with the validation step!',
+            prs_file_prefix + '_LDpred-inf.txt',
+            'test_data/goldens/goldenprs_LDpred-inf.txt',
+            prs_file_prefix + '_LDpred_p1.0000e-03.txt',
+            'test_data/goldens/goldenprs_LDpred_p1.0000e-03.txt')
+
+        prs_file_prefix2 = tmp_file_prefix + 'prs_2'
+        run_test(
+            'Validating results with output file prefix: %s' % tmp_file_prefix,
+            'score --gf=./test_data/LDpred_data_p0.001_test_0  --rf=%s  --out=%s' % (tmp_file_prefix, prs_file_prefix2),
+            'Problems with the validation step!',
+            prs_file_prefix2 + '_LDpred-inf.txt',
+            'test_data/goldens/goldenprs_2_LDpred-inf.txt',
+            prs_file_prefix2 + '_LDpred_p1.0000e-03.txt',
+            'test_data/goldens/goldenprs_2_LDpred_p1.0000e-03.txt',)
+
+        run_test(
+            'Validating results with output file prefix: %s' % tmp_file_prefix,
+            'score --gf=./test_data/LDpred_data_p0.001_test_0  --rf=%s  --rf-format=P+T --out=%s' % (tmp_file_prefix, prs_file_prefix),
+            'Problems with the P+T validation step!',
+            prs_file_prefix + '_P+T_p1.0000e-03.txt',
+            'test_data/goldens/goldenprs_2_P+T_p1.0000e-03.txt')
+
+        prs_file_prefix3 = tmp_file_prefix + 'prs_3'
+        run_test(
+            'Validating results with output file prefix: %s' % tmp_file_prefix,
+            'score --gf=./test_data/LDpred_data_p0.001_test_0  --only-score --rf=%s  --rf-format=P+T --out=%s' % (tmp_file_prefix, prs_file_prefix3),
+            'Problems with the P+T validation step!',
+            prs_file_prefix3 + '_P+T_p1.0000e-03.txt',
+            'test_data/goldens/goldenprs_3_P+T_p1.0000e-03.txt')
+        print('Test finished successfully!')
+
+    except Exception as e:
+        print("Test failed: ",e)
+        print('Cleaning up files.')
+        cmd_str = 'rm %s*' % tmp_file_prefix
+        print(cmd_str + '\n')
+        assert os.system(cmd_str) == 0, 'Problems cleaning up test files!  Testing stopped'
+        raise Exception('Test failed.')
+
     print('Cleaning up files.')
     cmd_str = 'rm %s*' % tmp_file_prefix
     print(cmd_str + '\n')
     assert os.system(cmd_str) == 0, 'Problems cleaning up test files!  Testing stopped'
-    raise Exception('Test failed.')
 
-print('Cleaning up files.')
-cmd_str = 'rm %s*' % tmp_file_prefix
-print(cmd_str + '\n')
-assert os.system(cmd_str) == 0, 'Problems cleaning up test files!  Testing stopped'
 
+if __name__ == '__main__':
+  unittest.main()
